9efd9efc38de4fc4c3d2e2ca3e4dbda7fd42c298
Johnson Khadka <er.johnsonkhadka@gmail.com>
2023-03-13 10:28:11 +0545

    React learning started 03/12

---
+++ testapp/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <John />
+      <h1>Welcome to E-Loksewa Online Platform</h1> 
+       <John />
+    </div>
+    
+  )
+}
+
+const John = () => {
+  return(
+    <div>
+      Mr. Johnson Khadka
+    </div>
+  )
+}
+
+export default App
