db6121baea32086f7ec91b5da2647fec4c22ae29
Johnson Khadka <er.johnsonkhadka@gmail.com>
2023-03-13 16:58:13 +0545

    navbar added, spread operator used to destructuring ,

--- testapp/src/component/Basics/MenuCard.js
+++ testapp/src/component/Basics/MenuCard.js
@@ -5,17 +5,19 @@ const MenuCard = ({ menuData }) => {
         <>
             <section className='main-card--cointainer'>
                 {menuData.map((currElem) => {
+
+                    const { id, name, image, description, category } = currElem
                     return (
                         <>
-                            <div className='card-container' key={currElem.id}>
+                            <div className='card-container' key={id}>
                                 <div className='card'>
                                     <div className='card-body'>
-                                        <span className='card-number card-circle subtle'>{currElem.id}</span>
-                                        <span className='card-author subtle'>{currElem.category}</span>
+                                        <span className='card-number card-circle subtle'>{id}</span>
+                                        <span className='card-author subtle'>{category}</span>
                                         <h2 className='card-title' >{currElem.name}</h2>
-                                        <span className='card-description subtle' >{currElem.description}</span>
+                                        <span className='card-description subtle' >{description}</span>
                                         <div className='card-read'>Read</div>
-                                        <img src='https://t3.ftcdn.net/jpg/02/02/21/78/240_F_202217885_SR8TwNqgjgSGFKGBe0LNGki8DccGvJXE.jpg' alt='images' className='card-media' />
+                                        {/* <img src='https://t3.ftcdn.net/jpg/02/02/21/78/240_F_202217885_SR8TwNqgjgSGFKGBe0LNGki8DccGvJXE.jpg' alt='images' className='card-media' /> */}
                                         <span className='card-tag subtle'>Order Now</span>
                                     </div>
                                 </div>
