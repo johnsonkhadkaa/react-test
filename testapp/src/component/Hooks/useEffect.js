06cf6197a98f1869a4731c63aa68d540849520f7
Johnson Khadka <er.johnsonkhadka@gmail.com>
2023-03-26 14:51:02 +0545

    useEffect hooks demo

---
+++ testapp/src/component/Hooks/useEffect.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState } from 'react'
+
+// const IncrementFunc = (curElem)=> {
+//     return setCount(curElem + 1)
+// }
+
+const UseEffect = () => {
+    const[count , setCount] = useState(0);
+
+    useEffect(()=>{
+      document.title = `Chats(${count})`
+    })
+
+  return (
+    <>
+    <div className='center-div'>
+    <p>{count}</p>
+    <div class='btn-group__item' onClick={() => setCount(count +1 )}>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    INCR
+    </div>
+    <div class='btn-group__item' onClick={() => count > 0 ? setCount(count - 1 ) : setCount(0)}>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    DECR
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default UseEffect;
