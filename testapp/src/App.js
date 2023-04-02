c24ff3984703742d883ce0b86d0780719afe6f98
Johnson Khadka <er.johnsonkhadka@gmail.com>
2023-04-02 15:32:15 +0545

    useReducer hooks implementation

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import Resturant from './component/Basics/Resturant'
+import UseReducer from './component/Hooks/useReducer'
+
+// import Resturant from './component/Basics/Resturant'
 // import UseState from './component/Hooks/UseState'
 // import UseEffect from './component/Hooks/useEffect'
 
 const App = () => {
+  return < UseReducer/>
   // return < UseState/>
   // return < UseEffect/>
-   return < Resturant/>
+  //  return < Resturant/>
 
 }
 
---
+++ testapp/src/component/Hooks/useReducer.js
@@ -0,0 +1,44 @@
+import React, { useReducer } from 'react'
+
+// const IncrementFunc = (curElem)=> {
+//     return setCount(curElem + 1)
+// }
+const reducer = (state , action)=>{
+  if(action.type === "INCR") {
+    state = state + 1
+  }
+  if(state > 0 && action.type === "DECR") {
+    state = state - 1
+  }
+  return state
+}
+
+const UseReducer = () => {
+    const initialData = 10
+   const[state , dispatch] = useReducer(reducer , initialData)
+   // const[count , setCount] = useState(0);
+
+  return (
+    <>
+    <div className='center-div'>
+    <p>{state}</p>
+    <div class='btn-group__item' onClick={() => dispatch({type : "INCR"}) }>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    INCR
+    </div>
+    <div class='btn-group__item' onClick={() => dispatch({type : "DECR"})}>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    DECR
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default UseReducer;
