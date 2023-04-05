88791c7c2d184f1e31283e4c41ec5e4251e7bc1a
Johnson Khadka <er.johnsonkhadka@gmail.com>
2023-04-05 15:39:17 +0545

    deleting task & remove all button works fine

--- testapp/src/component/todoreact/todo.js
+++ testapp/src/component/todoreact/todo.js
@@ -6,13 +6,34 @@ const Todo = () => {
     const [inputData, setInputData] = useState(" ")
     const [items, setItems] = useState([])
 
+    // To add the Items
     const addItem = () => {
         if (!inputData) {
             alert("Please add the items.")
         } else {
-            setItems([...items, inputData])
+           const myNewInputData = {
+                 id : new Date().getTime().toString(),
+                 name : inputData,
+           }
+            setItems([...items, myNewInputData])
+            setInputData("")
         }
     }
+
+// To delete the items
+    const deleteItem = (index) => {
+        const updatedItems = items.filter((currElem)=>{
+            return currElem.id !== index
+        })
+        setItems(updatedItems)
+    }
+
+// To remove all items
+     const removeAllItems = () => {
+        setItems([])
+     }
+
+
     return (
         <>
             <div className='main-div'>
@@ -24,20 +45,22 @@ const Todo = () => {
                     <div className='addItems'>
                         <input
                             type="text"
-                            placeholder='✍ Add Item' className='form-control' value={inputData}
+                            placeholder='✍ Add Item' 
+                            className='form-control'
+                            value={inputData}
                             onChange={(event) => setInputData(event.target.value)}
                         />
-                        <i className="fa fa-plus" add-btn onClick={addItem} ></i>
+                        <i className="fa fa-plus add-btn" onClick={addItem} ></i>
                     </div>
                     {/* Show our Items */}
                     <div className='showItems'>
-                        {items.map((currElem, index) => {
+                        {items.map((currElem) => {
                             return (
-                                <div className='eachItem'>
-                                    <h3>{currElem}</h3>
+                                <div className='eachItem' key={currElem.id}>
+                                    <h3>{currElem.name}</h3>
                                     <div className='todo-btn'>
-                                        <i className="far fa-edit" add-btn></i>
-                                        <i className="far fa-trash-alt" add-btn></i>
+                                        <i className="far fa-edit add-btn "></i>
+                                        <i className="far fa-trash-alt add-btn" onClick={()=>deleteItem(currElem.id)} ></i>
                                     </div>
                                 </div>
                             )
@@ -48,7 +71,7 @@ const Todo = () => {
 
                     {/* Remove Items */}
                     <div className='showItems'>
-                        <button className='btn effect04' data-sm-link-text='Remove All'>
+                        <button className='btn effect04' data-sm-link-text='Remove All' onClick={removeAllItems}>
                             <span>CHECK ITEMS</span>
                         </button>
                     </div>
