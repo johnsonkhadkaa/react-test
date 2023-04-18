f2414a583b36640468b4d3004dac17ecf15e2e82
Johnson Khadka <er.johnsonkhadka@gmail.com>
2023-04-18 15:55:57 +0545

    counter created to increment & decrement count

--- testapp/src/component/Counter/Counter.js
+++ testapp/src/component/Counter/Counter.js
@@ -1,17 +1,22 @@
+import { useState } from 'react'
 import React from 'react'
 import './style.css'
 
 const Counter = () => {
+
+  const[count , setCount] = useState(0);
+
   return (
     <>
       <div >
         <div className='child-div'>
-          <div className='card-read:after btn-group__item'><button>Increment</button></div>
+        <h1 className='fa '>{count}</h1>
+          <div className='card-read:after btn-group__item' onClick={() => setCount(count +1 )}><button>Increment</button></div>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
-          <div className='card-read:after btn-group__item'><button>Decrement</button></div>
+          <div className='card-read:after btn-group__item' onClick={() => count > 0 ? setCount(count -1 ) : setCount(0)}><button>Decrement</button></div>
         </div>
       </div>
     </>
